824672a38f4a57e295045057b5300865da428518
Rasagna26 <vemishettyrasagna@gmail.com>
2025-10-10 21:57:31 +0530

    Create selfdividingnumbers.py

---
+++ selfdividingnumbers.py
@@ -0,0 +1,16 @@
+
+
+from typing import List
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        res = []
+        for i in range(left, right + 1):
+            is_self_dividing = True
+            for digit in str(i):
+                if digit == '0' or i % int(digit) != 0:
+                    is_self_dividing = False
+                    break
+            if is_self_dividing:
+                res.append(i)
+        return res  # Correctly placed outside the loop
