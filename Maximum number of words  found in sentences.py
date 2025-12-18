ef2af4c7cc48f994406ecc0f6ff2f2a1799fe20c
Rasagna26 <vemishettyrasagna@gmail.com>
2025-12-18 17:42:46 +0530

    Create Maximum number of words  found in sentences.py

---
+++ Maximum number of words  found in sentences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        max_words = 0
+        
+        for s in sentences:
+            words = s.count(" ") + 1
+            max_words = max(max_words, words)
+        
+        return max_words
