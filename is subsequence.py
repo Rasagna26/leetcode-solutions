5851c8b54f20c331e9d8490ffd3b0c66689b5517
Rasagna26 <vemishettyrasagna@gmail.com>
2026-03-02 21:13:09 +0530

    Create is subsequence.py

---
+++ is subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for ch in s:
+            if ch in t:
+                t = t[t.index(ch)+1:]
+            else:
+                return False
+        return True
