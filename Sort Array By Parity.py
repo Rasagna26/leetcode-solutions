2ab16fbf52e7c1f442193f689b074f499b49b16d
Rasagna26 <vemishettyrasagna@gmail.com>
2026-03-21 20:20:02 +0530

    Create Sort Array By Parity.py

---
+++ Sort Array By Parity.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        res = []
+
+        for i in nums:
+            if i % 2 == 0:
+                res.append(i)
+
+        for i in nums:
+            if i % 2 != 0:
+                res.append(i)
+
+        return res
