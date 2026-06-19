df4b92349f6ea24aa495b7cdb26ec82b452b6872
Rasagna26 <vemishettyrasagna@gmail.com>
2026-06-19 18:32:34 +0530

    Create Maximum product subarray.py

---
+++ Maximum product subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        pre=1
+        suf=1
+        ans=float("-inf")
+        n=len(nums)
+        for i in range(n):
+            if pre==0:
+                pre=1
+            if suf==0:
+                suf=1
+            pre*=nums[i]
+            suf*=nums[n-i-1]
+            ans=max(ans,max(pre,suf))
+        return ans
